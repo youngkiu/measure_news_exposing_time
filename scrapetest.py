02ffb4a57cb3101d27911ba752c11dd93a8226ee
youngki lyu <youngkiu@gmail.com>
2017-04-05 10:45:59 +0900

    modularize functions

--- scrapetest.py
+++ scrapetest.py
@@ -1,38 +1,87 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-urlList = ("http://m.daum.net/", "http://m.naver.com/")
+import threading
 
-for url in urlList:
-    print("Open", url)
 
-    html = urlopen(url)
-
+def saveWebPage(url):
     filename = url
     filename = filename.replace("/", "")
     filename = filename.replace("http:", "")
     filename = filename.replace("https:", "")
-    filename = filename + ".txt"
+    filename = filename + ".html"
     
     print("Write", filename)
 
-    file = open(filename, 'wt')
-    file.write(html.read().decode('utf-8'))
+    file = open(filename, 'wb')
+    file.write(urlopen(url).read())
     file.close()
 
-    html = urlopen(url)
+def checkDaumNews(newsDic):
+    url = "http://m.daum.net/"
+    
+    saveWebPage(url)
 
+    html = urlopen(url)
     bsObj = BeautifulSoup(html.read(), "html.parser")
 
-    if url.find("daum") > 0:
-        newsTop = bsObj.find(id="channel_news1_top")
-        newsItems = newsTop.div.ul
-        newsText = newsItems.get_text()
-        newsArray = newsText.splitlines()
-        newsTitle = []
-        for news in newsArray:
-            if (news + ' ').isspace() == False:
-                newsTitle.append(news)
-                print(news)
+    newsTop = bsObj.div.find(id="channel_news1_top")
+    newsItems = newsTop.div.ul
+    newsText = newsItems.get_text()
+    newsArray = newsText.splitlines()
+    for news in newsArray:
+        if (news + ' ').isspace() == False:
+            if news in newsDic.keys():
+                value = newsDic.pop(news)
+                value += 1
+            else:
+                value = 1
+            newsDic[news] = value
+
+def checkNaverNews(newsDic):
+    url = "http://m.naver.com/"
+    
+    saveWebPage(url)
+
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html.read(), "html.parser")
+
+daumNews = {}
+naverNews = {}
+
+period = 30 * 1
+count = 0
+
+def onTimer():
+    global daumNews
+    global naverNews
+
+    global period
+    global count
+
+    count += 1
+    print(count, "th", "Check")
+    
+    checkDaumNews(daumNews)
+    checkNaverNews(naverNews)
 
     print("")
+
+    timer = threading.Timer(period, onTimer)
+    timer.start()
+
+    print("Accumulated time of Daum news")
+    for news in daumNews:
+        print(daumNews[news], news)
+    print("")
+
+    print("Accumulated time of Naver news")
+    for news in naverNews:
+        print(naverNews[news], news)
+    print("")
+
+
+print("Program Start")
+onTimer()
+print("Program End")
+
